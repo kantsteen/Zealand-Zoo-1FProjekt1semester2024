b6f605d2d57ca1ef7255aace1036464ade8ade7e
Mikkel Thomsen <mikk52j0@edu.zealand.dk>
2024-05-14 11:17:15 +0200

    UpdateEvent cs

--- Pages/Events/UpdateEvent.cshtml.cs
+++ Pages/Events/UpdateEvent.cshtml.cs
@@ -5,8 +5,25 @@ namespace Zealand_Zoo_1FProjekt1semester2024.Pages.Events
 {
     public class UpdateEventModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Event Event { get; set; }
+        private IEventRepository catalog;
+        public UpdateEventModel(IEventRepository repository)
         {
+            catalog = repository;
         }
-    }
+        public void OnGet(int id)
+        {
+            Event = catalog.GetEvent(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            catalog.UpdateEvent(Event);
+            return RedirectToPage("ReadEvent");
+        }
 }
