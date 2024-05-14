f7cd80d0c9a695b55767d252f67475c3d63fab7d
Dasekan Allan Karim <dak003@edu.zealand.dk>
2024-05-14 13:54:34 +0200

    leggo

--- Pages/Events/CreateEvent.cshtml.cs
+++ Pages/Events/CreateEvent.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zealand_Zoo_1FProjekt1semester2024.Interface;
+using Zealand_Zoo_1FProjekt1semester2024.Models;
 
 namespace Zealand_Zoo_1FProjekt1semester2024.Pages.Events
 {
     public class CreateEventModel : PageModel
     {
-        public void OnGet()
+        private IEventRepository catalog;
+        [BindProperty]
+        public Event Event { get; set; }
+
+        public CreateEventModel(IEventRepository evt)
         {
+            catalog = evt;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            catalog.AddEvent(Event);
+
+            return RedirectToPage("ShowAllPizzas");
         }
     }
 }
