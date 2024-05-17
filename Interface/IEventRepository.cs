b15ad12d4b8a72086a9a2f0ba32c449eeab7b366
Lasse Egebo <lae003@edu.zealand.dk>
2024-05-17 09:55:46 +0200

    Event bitch

--- Interface/IEventRepository.cs
+++ Interface/IEventRepository.cs
@@ -8,7 +8,7 @@ namespace Zealand_Zoo_1FProjekt1semester2024.Interface
 
         Dictionary<int, Event> FilterEvent(string crtiteria);
 
-        void DeleteEvent(int id);
+        void DeleteEvent(Event evt);
 
         void AddEvent(Event evt);
 
