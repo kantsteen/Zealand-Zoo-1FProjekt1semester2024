4de41b8b8373c6b3ba159b8c70f529a72ce0779a
Mikkel Thomsen <mikk52j0@edu.zealand.dk>
2024-05-21 14:46:18 +0200

    oink

---
+++ Pages/Login/BasePageModel.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zealand_Zoo_1FProjekt1semester2024.Pages.Login
+{
+    public class BasePageModelModel : PageModel
+    {
+        public string Name => HttpContext.Session.GetString("Name");
+
+        public bool IsAuthenticated => !string.IsNullOrEmpty(Name);
+
+        public void CheckLogin()
+        {
+            if (!IsAuthenticated)
+            {
+                Response.Redirect("/Login");
+            }
+        }
+    }
+}
